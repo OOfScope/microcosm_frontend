6ecc243842fe366e6bebfd38ec7e7f6d73c70e32
Theviki20110 <vincenzolapadula23@gmail.com>
2024-05-12 17:51:58 +0200

    refactor

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:admin/screens/dashboard/components/header.dart';
+import 'package:admin/screens/main/components/header.dart';
 import '../../constants.dart';
 
 class ProfileScreen extends StatelessWidget {
