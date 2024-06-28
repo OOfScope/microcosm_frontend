2653fe0b7f2aa45a8085b89337db6c267c47f5cd
mich2k <inbox@gmichele.com>
2024-06-28 14:04:59 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,10 @@ Future<Map<String, dynamic>> jwt_decode(String token) async {
 
   final response = await http.get(Uri.parse(jwt_decode_url));
   if (response.statusCode != 200) {
-    console.log('Error in token parsing!'.toJS);
+    print('ERR: Error in token parsing!');
     return {};
   } else {
-    String body = response.body;
-    var ret = jsonDecode(body) as Map<String, dynamic>;
+    var ret = jsonDecode(response.body) as Map<String, dynamic>;
     return ret;
   }
 }
@@ -40,19 +39,22 @@ void main() async {
     });
     final cookieMap = Map.fromEntries(entity);
     final String? token = cookieMap["CF_Authorization"];
-    console.log(token?.toJS);
+    print(token);
 
     if (token != null) {
       decodedToken = await jwt_decode(token);
     }
   } else {
+
+    print('WARN: Using test-token!');
+
     final String test_token =
         "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5OTE0OTI4ZWE0MzkwZjEyYzY3MzdmZWFhYWRmYTA1NTcwMDlhMjE0OTVhYzE4NTExMTIzYzgzYjhiM2QxN2EifQ.eyJhdWQiOlsiOTkzYmQ1ZDY3YmQ3NTFiNTQ1YmY4N2I5MzY1MWRlOTBiMmU0ODI2NTAzZDgzMTRhMjg3N2NlZjVkMzgwOWYxYyJdLCJlbWFpbCI6IjI3MzgzOUBzdHVkZW50aS51bmltb3JlLml0IiwiZXhwIjoxNzE4OTc5MDg0LCJpYXQiOjE3MTg5NzcyODQsIm5iZiI6MTcxODk3NzI4NCwiaXNzIjoiaHR0cHM6Ly9nbWljaGVsZS5jbG91ZGZsYXJlYWNjZXNzLmNvbSIsInR5cGUiOiJhcHAiLCJpZGVudGl0eV9ub25jZSI6IjJha3doSjlabnhmZnY1SE8iLCJzdWIiOiJkZDQyMjllNy0yMDgzLTVkNWYtYTU1NC04MmQ4MTAxMGMwNmIiLCJjb3VudHJ5IjoiSVQifQ.XvCybleXeWHMGHx_TZOSzHc9LcqEf0yZfd3u5Uo0eMstR1hiT1qjYjsDstaCvpPBz7EZQLOHNIhycxKCXfKcesRvqDRwHQYkQvbCjmhMMbT6a0HYlcvxyAEO8fJrOcdL8gMqaCg4Pf08mNuCx-vbFdWkYcHfbbQ_2ehDeLK59GHIshuOAy7y6H6RYKp6I2nqz8drBUVm3koyIW3Pk3iyqsx6_gcqpMblNBZoRWz9mznKpoR1gQ6WvifnzPkzxz3uLz9WXjxlLrS-UAu0TKzm-qejnhUyVDW5aCCXm2cd04-t16f36QDWclS9KkCutOHof8JCOCxgPlmXxb43V72tiw";
     decodedToken = await jwt_decode(test_token);
   }
 
   if (decodedToken == null || decodedToken.isEmpty) {
-    console.log('Error in token parsing or token is empty!'.toJS);
+    console.log('ERR: Error in token parsing or token is empty!'.toJS);
   } else {
     print(decodedToken);
   }
