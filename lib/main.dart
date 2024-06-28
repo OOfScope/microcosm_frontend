5b5e24a97600c6a7e103275ae448a11cf9b26395
Theviki20110 <vincenzolapadula23@gmail.com>
2024-06-28 13:54:19 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -12,54 +12,57 @@ import 'package:provider/provider.dart';
 import 'package:web/web.dart';
 import 'package:http/http.dart' as http;
 
-
-Future<Map<String,dynamic>> jwt_decode(String token) async {
+Future<Map<String, dynamic>> jwt_decode(String token) async {
   final String jwt_decode_url =
-      'https://microcosm-backend.gmichele.com/parse_jwt/user_data/' + token;
-  
+      'https://microcosm-backend.gmichele.com/parse_jwt/user_data/$token';
+
   final response = await http.get(Uri.parse(jwt_decode_url));
-  if (response.statusCode != 200){
+  if (response.statusCode != 200) {
     console.log('Error in token parsing!'.toJS);
-    return new Map<String,dynamic>();
-  }else{
-      String body = response.body;
-      var ret = jsonDecode(response.body) as Map<String, dynamic>;
-      print(ret);
-
-      return ret;
-
+    return {};
+  } else {
+    String body = response.body;
+    var ret = jsonDecode(body) as Map<String, dynamic>;
+    //console.dir(ret.jsify());
+    return ret;
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  dynamic ret;
+  Map<String, dynamic>? decodedToken;
+
   if (kReleaseMode) {
-    print('K-RELEASE');
-
     final cookie = document.cookie;
     final entity = cookie.split("; ").map((item) {
       final split = item.split("=");
       return MapEntry(split[0], split[1]);
     });
     final cookieMap = Map.fromEntries(entity);
-    final String token = cookieMap["CF_Authorization"].toString();
+    final String? token = cookieMap["CF_Authorization"];
+    console.log(token?.toJS);
 
-    ret = jwt_decode(token);
-
-  }else{
-    final String test_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5OTE0OTI4ZWE0MzkwZjEyYzY3MzdmZWFhYWRmYTA1NTcwMDlhMjE0OTVhYzE4NTExMTIzYzgzYjhiM2QxN2EifQ.eyJhdWQiOlsiOTkzYmQ1ZDY3YmQ3NTFiNTQ1YmY4N2I5MzY1MWRlOTBiMmU0ODI2NTAzZDgzMTRhMjg3N2NlZjVkMzgwOWYxYyJdLCJlbWFpbCI6IjI3MzgzOUBzdHVkZW50aS51bmltb3JlLml0IiwiZXhwIjoxNzE4OTc5MDg0LCJpYXQiOjE3MTg5NzcyODQsIm5iZiI6MTcxODk3NzI4NCwiaXNzIjoiaHR0cHM6Ly9nbWljaGVsZS5jbG91ZGZsYXJlYWNjZXNzLmNvbSIsInR5cGUiOiJhcHAiLCJpZGVudGl0eV9ub25jZSI6IjJha3doSjlabnhmZnY1SE8iLCJzdWIiOiJkZDQyMjllNy0yMDgzLTVkNWYtYTU1NC04MmQ4MTAxMGMwNmIiLCJjb3VudHJ5IjoiSVQifQ.XvCybleXeWHMGHx_TZOSzHc9LcqEf0yZfd3u5Uo0eMstR1hiT1qjYjsDstaCvpPBz7EZQLOHNIhycxKCXfKcesRvqDRwHQYkQvbCjmhMMbT6a0HYlcvxyAEO8fJrOcdL8gMqaCg4Pf08mNuCx-vbFdWkYcHfbbQ_2ehDeLK59GHIshuOAy7y6H6RYKp6I2nqz8drBUVm3koyIW3Pk3iyqsx6_gcqpMblNBZoRWz9mznKpoR1gQ6WvifnzPkzxz3uLz9WXjxlLrS-UAu0TKzm-qejnhUyVDW5aCCXm2cd04-t16f36QDWclS9KkCutOHof8JCOCxgPlmXxb43V72tiw";
-
-    ret = jwt_decode(test_token);
-
+    if (token != null) {
+      decodedToken = await jwt_decode(token);
+    }
+  } else {
+    final String test_token =
+        "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5OTE0OTI4ZWE0MzkwZjEyYzY3MzdmZWFhYWRmYTA1NTcwMDlhMjE0OTVhYzE4NTExMTIzYzgzYjhiM2QxN2EifQ.eyJhdWQiOlsiOTkzYmQ1ZDY3YmQ3NTFiNTQ1YmY4N2I5MzY1MWRlOTBiMmU0ODI2NTAzZDgzMTRhMjg3N2NlZjVkMzgwOWYxYyJdLCJlbWFpbCI6IjI3MzgzOUBzdHVkZW50aS51bmltb3JlLml0IiwiZXhwIjoxNzE4OTc5MDg0LCJpYXQiOjE3MTg5NzcyODQsIm5iZiI6MTcxODk3NzI4NCwiaXNzIjoiaHR0cHM6Ly9nbWljaGVsZS5jbG91ZGZsYXJlYWNjZXNzLmNvbSIsInR5cGUiOiJhcHAiLCJpZGVudGl0eV9ub25jZSI6IjJha3doSjlabnhmZnY1SE8iLCJzdWIiOiJkZDQyMjllNy0yMDgzLTVkNWYtYTU1NC04MmQ4MTAxMGMwNmIiLCJjb3VudHJ5IjoiSVQifQ.XvCybleXeWHMGHx_TZOSzHc9LcqEf0yZfd3u5Uo0eMstR1hiT1qjYjsDstaCvpPBz7EZQLOHNIhycxKCXfKcesRvqDRwHQYkQvbCjmhMMbT6a0HYlcvxyAEO8fJrOcdL8gMqaCg4Pf08mNuCx-vbFdWkYcHfbbQ_2ehDeLK59GHIshuOAy7y6H6RYKp6I2nqz8drBUVm3koyIW3Pk3iyqsx6_gcqpMblNBZoRWz9mznKpoR1gQ6WvifnzPkzxz3uLz9WXjxlLrS-UAu0TKzm-qejnhUyVDW5aCCXm2cd04-t16f36QDWclS9KkCutOHof8JCOCxgPlmXxb43V72tiw";
+    decodedToken = await jwt_decode(test_token);
   }
-  print(ret);
+
+  if (decodedToken == null || decodedToken.isEmpty) {
+    console.log('Error in token parsing or token is empty!'.toJS);
+  } else {
+    print(decodedToken['email']);
+  }
+
   debugPaintSizeEnabled = false;
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
