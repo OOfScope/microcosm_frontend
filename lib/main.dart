498be2c8f029f2e31bccbf240f49c8996bfa1826
mich2k <inbox@gmichele.com>
2024-06-28 13:47:20 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ Future<Map<String,dynamic>> jwt_decode(String token) async {
 
 void main() {
 
-
+  dynamic ret;
   if (kReleaseMode) {
     print('K-RELEASE');
 
@@ -44,16 +44,16 @@ void main() {
     });
     final cookieMap = Map.fromEntries(entity);
     final String token = cookieMap["CF_Authorization"].toString();
-    console.log(token.toJS);
 
-    dynamic ret = jwt_decode(token);
+    ret = jwt_decode(token);
 
   }else{
     final String test_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5OTE0OTI4ZWE0MzkwZjEyYzY3MzdmZWFhYWRmYTA1NTcwMDlhMjE0OTVhYzE4NTExMTIzYzgzYjhiM2QxN2EifQ.eyJhdWQiOlsiOTkzYmQ1ZDY3YmQ3NTFiNTQ1YmY4N2I5MzY1MWRlOTBiMmU0ODI2NTAzZDgzMTRhMjg3N2NlZjVkMzgwOWYxYyJdLCJlbWFpbCI6IjI3MzgzOUBzdHVkZW50aS51bmltb3JlLml0IiwiZXhwIjoxNzE4OTc5MDg0LCJpYXQiOjE3MTg5NzcyODQsIm5iZiI6MTcxODk3NzI4NCwiaXNzIjoiaHR0cHM6Ly9nbWljaGVsZS5jbG91ZGZsYXJlYWNjZXNzLmNvbSIsInR5cGUiOiJhcHAiLCJpZGVudGl0eV9ub25jZSI6IjJha3doSjlabnhmZnY1SE8iLCJzdWIiOiJkZDQyMjllNy0yMDgzLTVkNWYtYTU1NC04MmQ4MTAxMGMwNmIiLCJjb3VudHJ5IjoiSVQifQ.XvCybleXeWHMGHx_TZOSzHc9LcqEf0yZfd3u5Uo0eMstR1hiT1qjYjsDstaCvpPBz7EZQLOHNIhycxKCXfKcesRvqDRwHQYkQvbCjmhMMbT6a0HYlcvxyAEO8fJrOcdL8gMqaCg4Pf08mNuCx-vbFdWkYcHfbbQ_2ehDeLK59GHIshuOAy7y6H6RYKp6I2nqz8drBUVm3koyIW3Pk3iyqsx6_gcqpMblNBZoRWz9mznKpoR1gQ6WvifnzPkzxz3uLz9WXjxlLrS-UAu0TKzm-qejnhUyVDW5aCCXm2cd04-t16f36QDWclS9KkCutOHof8JCOCxgPlmXxb43V72tiw";
 
-    dynamic ret = jwt_decode(test_token);
+    ret = jwt_decode(test_token);
 
   }
+  print(ret);
   debugPaintSizeEnabled = false;
   runApp(MyApp());
 }
